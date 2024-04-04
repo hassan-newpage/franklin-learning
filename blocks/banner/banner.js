72aa46a141e5149d8b3806c769c2eab4e23332a1
Hassan Mohammed Nabeel <hassan.nabeel@newpage.io>
2024-04-04 22:32:50 +0530

    accordian, help resource, share story

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,7 +1,13 @@
+import { fetchPlaceholders } from "../../scripts/aem.js";
 
 export default async function decorate(block) {
     const ParentDiv = block.querySelector('.banner.block > div:first-child');
     const firstDiv = block.querySelector('.banner.block > div > div:first-child');
     firstDiv.classList.add('text-holder');
     ParentDiv.classList.add('parent-container');
+
+
+    // place holder
+    const addKey = await fetchPlaceholders();
+    firstDiv.children[0].innerText = addKey.add;
 }
